c4f180b5bd71f5787fd6fe0110662aed3ea0173a
Kumaran R <37325822+kumar1987an@users.noreply.github.com>
2022-01-16 13:25:23 +0000

    struct change

--- main.py
+++ main.py
@@ -1,1 +1,8 @@
-from Basics._2_Variables import code
+# from Basics._2_Variables import code
+from Basics._3_Objects_in_python import code
+# from Basics._4_Indentation import code
+# from Basics._5_Name_Spaces import code
+# from Basics._6_Data_Types import code
+# from Basics._7_Operator_Precedence import code
+# from Basics._8_Conditionals import code
+# from Basics._9_Loops import code
